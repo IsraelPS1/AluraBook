c7e0324fae7ca67e7f0acd149456af83139871fe
IsraelPS1 <103294241+IsraelPS1@users.noreply.github.com>
2023-03-20 19:44:45 -0300

    Adicionando novas funcionalidades pros cards

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ const swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 4000,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "coverflow",
 });
